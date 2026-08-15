562ffbb78e2e98200422fd0a3c4792a9a34a94f4
RodDyg118 <rodresscoldskj@gmail.com>
2026-08-14 22:21:33 -0300

    Notes

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace Notes;
+
+public partial class MainPage : ContentPage
+{
+
+    string caminho = Path.Combine (FileSystem.AppDataDirectory, "nota.txt"); 
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void AoClicarSalvar(object sender, EventArgs e)
+    {
+        string conteudo = NotasEditor.Text;
+        File.WriteAllText(caminho, conteudo);
+    }
+
+    private void AoClicarApagar(object sender, EventArgs e)
+    {
+    }
+
+    private void AoClicarCarregar(object sender, EventArgs e)
+    {
+    }
+
+}
